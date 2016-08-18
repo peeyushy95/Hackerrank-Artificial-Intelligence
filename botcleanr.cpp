904ded652923e7d251bb83f47f8f74b376e1ac63
Peeyush Yadav <peeyushy95@gmail.com>
2016-08-18 16:14:28 +0530

    BOT BUILD

---
+++ botcleanr.cpp
@@ -0,0 +1,89 @@
+/*
+   (o_o)
+   <) )>
+    / \
+   AUTHOR  : Peeyush Yadav
+   Problem : https://www.hackerrank.com/challenges/botcleanr
+*/
+
+#include<bits/stdc++.h>
+using namespace std; typedef long long ll; typedef pair<int,int> grp;
+inline ll power(ll a,ll b)     { ll r=1; while(b){ if(b&1) r=r*a    ; a=a*a    ; b>>=1;} return r;}
+inline ll power(ll a,ll b,ll m){ ll r=1; while(b){ if(b&1) r=(r*a)%m; a=(a*a)%m; b>>=1;} return r;}
+void fast(){	
+	#ifdef Megamind
+		freopen("inp.txt","r",stdin);
+		//freopen("out.txt","w",stdout);
+		#define trace(x)            cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<endl;
+		#define trace2(x,y)         cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<" | "#y" = "<<y<<endl;
+		#define trace3(x,y,z)       cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<" | "#y" = "<<y<<" | "#z" = "<<z<<endl;
+	#else
+		#define trace(x)
+		#define trace2(x,y)
+		#define trace3(x,y,z)
+	#endif
+	
+	#define fi						first
+	#define se						second
+	#define mp						make_pair
+	#define pb(x)					push_back(x)
+	#define s(x)					scanf("%d",&x);
+	#define sl(x)					scanf("%lld",&x);
+	#define p(x)					printf("%d\n",x);
+	#define f(a,b,c)				for(int a=b;a<c;a++)
+	//#define r(a,b,c)				for(int a=b;a>c;a--)
+	#define p2(x,y)					printf("%d %d\n",x,y);
+	#define pl(x)					printf("%lld\n",x);
+	#define pl2(x,y)				printf("%lld %lld\n",x,y);
+	#define p1d(a,n)				for(int ix=0;ix<n;ix++) printf("%d ",a[ix]); printf("\n");
+	#define p2d(a,n,m)				for(int ix=0;ix<n;ix++){ for(int jx=0;jx<m;jx++) printf("%d ",a[ix][jx]); printf("\n");}
+	#define TimeElapsed				(double)clock()/CLOCKS_PER_SEC
+}
+/*................................................END OF TEMPLATES............................................................*/
+
+
+
+void nextMove(int posr, int posc, vector <string> board) {
+    //add logic here
+    int dst = 20,b,v,tm;
+    if(board[posr][posc] == 'd') cout<<"CLEAN";
+    else{
+        for(int i=0;i<5;i++) 
+            for(int j=0;j<5;j++) {
+               if(board[i][j] == 'd'){
+                   tm = abs(i-posr) + abs(j- posc);
+                   if(tm < dst){
+                       dst = tm;
+                       b = i;
+                       v = j;
+                   }
+               }
+        }
+          if(b > posr){
+              cout<<"DOWN";
+          }
+        else if(b < posr){
+            cout<<"UP";
+        }
+        else if(v > posc){
+            cout<<"RIGHT";
+        }
+        else if(v < posc){
+            cout<<"LEFT";
+        }
+    }
+}
+
+
+int main(void) {
+    int pos[2];
+    vector <string> board;
+    cin>>pos[0]>>pos[1];
+    for(int i=0;i<5;i++) {
+        string s;
+        cin >> s;
+        board.push_back(s);
+    }
+    nextMove(pos[0], pos[1], board);
+    return 0;
+}
